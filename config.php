b9ca27e19724129122be16a311bbd4a965d6e383
Matthew Faulkner <matthewnfaulkner@gmail>
2023-02-14 18:40:13 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * The configuration for theme_apoa is defined here.
+ *
+ * @package     theme_apoa
+ * @copyright   2023 Matthew Faulkner matthewnfaulkner@gmail.com
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+
+// $THEME is defined before this page is included and we can define settings by adding properties to this global object.            
+                                                                                                                                    
+// The first setting we need is the name of the theme. This should be the last part of the component name, and the same             
+// as the directory name for our theme.                                                                                             
+$THEME->name = 'apoa';                                                                                                             
+                                                                                                                                    
+// This setting list the style sheets we want to include in our theme. Because we want to use SCSS instead of CSS - we won't        
+// list any style sheets. If we did we would list the name of a file in the /style/ folder for our theme without any css file      
+// extensions.                                                                                                                      
+$THEME->sheets = [];                                                                                                                
+$THEME->layouts = [
+    // Most backwards compatible layout without the blocks.
+
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'landing.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nonavbar' => false),
+    )
+];                                                              
+$THEME->editor_sheets = [];                                                                                                                                                                              
+$THEME->parents = ['boost'];                                                                                                                        
+$THEME->enable_dock = false;                                                                                                                          
+$THEME->yuicssmodules = array();                                                                                                       
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';                                                                              
+$THEME->requiredblocks = '';   
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+$THEME->precsscallback = 'theme_apoa_get_pre_scss';
+//$THEME->removedprimarynavitems = ['courses'];
+
+
+$THEME->scss = function($theme) {
+    return theme_apoa_get_main_scss_content($theme);
+};
+
---
+++ lib.php
@@ -0,0 +1,56 @@
+<?php
+
+// Every file should have GPL and copyright in the header - we skip it in tutorials but you should not skip it for real.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+
+// We will add callbacks here as we add features to our theme.
+
+
+function theme_apoa_get_main_scss_content($theme) {                                                                                
+    global $CFG;                                                                                                                    
+                                                                                                                                    
+    $scss = '';                                                                                                                     
+    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;                                                 
+    $fs = get_file_storage();                                                                                                       
+                                                                                                                                    
+    $context = context_system::instance();                                                                                          
+    if ($filename == 'default.scss') {                                                                                              
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
+    } else if ($filename == 'plain.scss') {                                                                                         
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');                                          
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_boost', 'preset', 0, '/', $filename))) {              
+        $scss .= $presetfile->get_content();                                                                                        
+    } else {                                                                                                                        
+        // Safety fallback - maybe new installs etc.                                                                                
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');                                        
+    }                                                                                                                               
+    
+    $pre = file_get_contents($CFG->dirroot . '/theme/apoa/scss/pre.scss');
+    $post = file_get_contents($CFG->dirroot . '/theme/apoa/scss/post.scss');
+    $booststudio = file_get_contents($CFG->dirroot . '/theme/apoa/scss/bootstudio.scss');
+    return $pre . "\n" . $scss . "\n" . $post . "\n" . $booststudio;                                                                                                                  
+}
+
+
+function theme_apoa_get_pre_scss($theme) {
+    // Load the settings from the parent.                                                                                           
+    $theme = theme_config::load('boost');                                                                                           
+    // Call the parent themes get_pre_scss function.                                                                                
+    return theme_boost_get_pre_scss($theme);                         
+}
+
+function theme_apoa_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array()) {
+    if ($context->contextlevel == CONTEXT_SYSTEM && ($filearea === 'logo' || $filearea === 'backgroundimage' ||
+        $filearea === 'loginbackgroundimage')) {
+        $theme = theme_config::load('boost');
+        // By default, theme files must be cache-able by both browsers and proxies.
+        if (!array_key_exists('cacheability', $options)) {
+            $options['cacheability'] = 'public';
+        }
+        return $theme->setting_file_serve($filearea, $args, $forcedownload, $options);
+    } else {
+        send_file_not_found();
+    }
+}
