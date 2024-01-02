a5175814e04c3f47e95ecd4383a90430d599ae04
Matthew Faulkner <matthewnfaulkner@gmail.com>
2024-01-02 19:15:24 +0800

    improved lti configuration

--- lti_configure.php
+++ lti_configure.php
@@ -138,19 +138,19 @@ foreach ($resources as $resource) {
                     if($resourcelink = $DB->get_record('freepapervote_resource_link', array('resourceid' => $resourceid->id), 'id')){
 
                         $id = $resourcelink->id;
-                        $resourcetags = $DB->get_records('freepapervote_resource_tags', array('resourceid' => $resourceid), 'tagid');
+                        /*$resourcetags = $DB->get_records('freepapervote_resource_tags', array('resourceid' => $resourceid), 'tagid');
 
                         if($tagstodelete = array_diff_key($resourcetags, $tagobjects)){
                             list($deletesql, $deleteparams) = $DB->get_in_or_equal(array_keys($tagstodelete));
                             $deleteparams[] = $id;
 
                             $DB->delete_records_select('freepapervote_resource_tags', "tagid $deletesql AND resourecid = ?", $deleteparams);
-                        }
+                        }*/
                         $freepapervote->id = $resourcelink->id;
                         $DB->update_record('freepapervote_resource_link', $freepapervote);
                     }
                     else{
-                        $id = $DB->insert_record('freepapervote_resource_link', $freepapervote);
+                        $id = $DB->insert_record('freepapervote_resource_link', $freepapervote);    
                     }
                 }
                 else{
