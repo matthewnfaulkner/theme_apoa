e2e072e59784b28ebb7cfc6094b30501649684b1
Matthew Faulkner <matthewnfaulkner@gmail.com>
2025-05-23 19:22:17 +0800

    overrode rss_client_block renderer

---
+++ classes/output/block_rss_client_renderer.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains class block_rss_client\output\block_renderer_html
+ *
+ * @package   block_rss_client
+ * @copyright 2015 Howard County Public School System
+ * @author    Brendan Anderson <brendan_anderson@hcpss.org>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace theme_apoa\output;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Renderer for RSS Client block
+ *
+ * @package   block_rss_client
+ * @copyright 2015 Howard County Public School System
+ * @author    Brendan Anderson <brendan_anderson@hcpss.org>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_rss_client_renderer extends \block_rss_client\output\renderer {
+
+    /**
+     * Render an RSS feeds block
+     *
+     * @param \templatable $block
+     * @return string
+     */
+    public function render_block(\templatable $block) {
+        $data = $block->export_for_template($this);
+
+        foreach($data['feeds'] as $key => $feeds) {
+            if(empty($feeds['items'])){
+                $data['feeds'][$key] = [];
+            }
+        }
+        $data['feeds'][0]['first'] = true;
+        
+        return $this->render_from_template('block_rss_client/block', $data);
+    }
+
+}
